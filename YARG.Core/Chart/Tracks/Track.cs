9450c3cd45eff42b92b4e617665e884c4814d5ab
sonicfind <sonicfind2@gmail.com>
2023-09-01 22:42:02 -0500

    Introduce base abstract Track class to hold events and special phrases

---
+++ YARG.Core/Chart/Tracks/Track.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace YARG.Core.Chart
+{
+    public abstract class Track
+    {
+        public TimedFlatDictionary<List<SpecialPhrase_FW>> specialPhrases = new();
+        public TimedFlatDictionary<List<string>> events = new();
+        public virtual bool IsOccupied() { return !specialPhrases.IsEmpty() || !events.IsEmpty(); }
+        public virtual void Clear()
+        {
+            specialPhrases.Clear();
+            events.Clear();
+        }
+        public abstract void TrimExcess();
+        public abstract long GetLastNoteTime();
+    }
+}
