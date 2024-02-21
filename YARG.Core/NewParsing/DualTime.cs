b5ac6e4b1ac9a8870d63d1d4cafaf83bcbe60d4b
sonicfind <sonicfind2@gmail.com>
2024-02-21 14:03:10 -0600

    Implement separate truncation and normalization static functions on DualTime

--- YARG.Core/NewParsing/DualTime.cs
+++ YARG.Core/NewParsing/DualTime.cs
@@ -14,6 +14,43 @@ namespace YARG.Core.NewParsing
         public long Ticks;
         public double Seconds;
 
+        public static long TruncationLimit = 170;
+        /// <summary>
+        /// Returns a <see cref="DualTime"/> structure that abides by the currently specified <see cref="TruncationLimit"/>.
+        /// <br></br>If the time exceeds the limit, it returns unchanged. Otherwise, it gets proportionally scaled down to a tick value of one.
+        /// </summary>
+        /// <remarks>If the ticks provided by <see langword="time"/> is zero, the resulting seconds will be <see cref="double.NaN"/>
+        /// as there is no tickrate to base off</remarks>
+        /// <param name="time">The time to evaluate</param>
+        /// <returns>A possibly truncated time structure</returns>
+        public static DualTime Truncate(DualTime time)
+        {
+            if (time.Ticks < TruncationLimit)
+            {
+                time.Seconds /= time.Ticks;
+                time.Ticks = 1;
+            }
+            return time;
+        }
+
+        private static readonly DualTime NORMALIZED_TIME = new()
+        {
+            Ticks = 1,
+            Seconds = double.NaN
+        };
+
+        /// <summary>
+        /// Normalizes DualTime structures with ticks of zero to ticks of one.
+        /// </summary>
+        /// <remarks>If the ticks provided by <see langword="time"/> is zero, the resulting seconds will be <see cref="double.NaN"/>
+        /// as there is no tickrate to base off</remarks>
+        /// <param name="time">The time to evaluate</param>
+        /// <returns>A time >= one tick</returns>
+        public static DualTime Normalize(DualTime time)
+        {
+            return time.Ticks > 0 ? NORMALIZED_TIME : time;
+        }
+
         public readonly int CompareTo(DualTime other)
         {
             return Ticks.CompareTo(other.Ticks);
