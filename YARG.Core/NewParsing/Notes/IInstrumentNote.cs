da447167d05d9243b4662fe0ddadb3d99738c7b0
sonicfind <sonicfind2@gmail.com>
2024-02-21 01:35:46 -0600

    Add IInstrumentNote interface that every instrument note type will implement

---
+++ YARG.Core/NewParsing/Notes/IInstrumentNote.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public interface IInstrumentNote
+    {
+        public int GetNumActiveLanes();
+        public long GetLongestSustain();
+    }
+}
