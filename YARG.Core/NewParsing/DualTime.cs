b69987ca049dd871470a16b0f3e67f16f4962e22
sonicfind <sonicfind2@gmail.com>
2024-02-21 01:44:02 -0600

    Add DualTime structure for precalculated time values
    DualTime handles tracking positions & durations in both ticks and seconds. This is instrumental to have as it minimizes the need to calculate relative positioning during runtime.

---
+++ YARG.Core/NewParsing/DualTime.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public struct DualTime : IEquatable<DualTime>, IComparable<DualTime>
+    {
+        public static readonly DualTime Inactive = new()
+        {
+            Ticks = -1,
+        };
+
+        public long Ticks;
+        public double Seconds;
+
+        public readonly int CompareTo(DualTime other)
+        {
+            return Ticks.CompareTo(other.Ticks);
+        }
+
+        public readonly bool Equals(DualTime other)
+        {
+            return Ticks.Equals(other.Ticks);
+        }
+
+        public readonly override bool Equals(object obj)
+        {
+            return obj is DualTime dual && Equals(dual);
+        }
+
+        public readonly override int GetHashCode()
+        {
+            return Ticks.GetHashCode();
+        }
+
+        public static bool operator <(in DualTime lhs, in DualTime rhs)
+        {
+            return lhs.Ticks < rhs.Ticks;
+        }
+
+        public static bool operator >(in DualTime lhs, in DualTime rhs)
+        {
+            return lhs.Ticks > rhs.Ticks;
+        }
+
+        public static bool operator <=(in DualTime lhs, in DualTime rhs)
+        {
+            return lhs.Ticks <= rhs.Ticks;
+        }
+
+        public static bool operator >=(in DualTime lhs, in DualTime rhs)
+        {
+            return lhs.Ticks >= rhs.Ticks;
+        }
+
+        public static bool operator ==(in DualTime lhs, in DualTime rhs)
+        {
+            return lhs.Ticks == rhs.Ticks;
+        }
+
+        public static bool operator !=(in DualTime lhs, in DualTime rhs)
+        {
+            return lhs.Ticks != rhs.Ticks;
+        }
+
+        public static DualTime operator -(in DualTime lhs, in DualTime rhs)
+        {
+            return new DualTime()
+            {
+                Ticks = lhs.Ticks - rhs.Ticks,
+                Seconds = lhs.Seconds - rhs.Seconds,
+            };
+        }
+
+        public static DualTime operator +(in DualTime lhs, in DualTime rhs)
+        {
+            return new DualTime()
+            {
+                Ticks = lhs.Ticks + rhs.Ticks,
+                Seconds = lhs.Seconds + rhs.Seconds,
+            };
+        }
+    }
+}
