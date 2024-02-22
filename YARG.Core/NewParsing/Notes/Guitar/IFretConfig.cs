24728e1f8b2e923c2b9311c9bb5bd9dc6b286f05
sonicfind <sonicfind2@gmail.com>
2024-02-22 09:40:53 -0600

    Implement generic GuitarNote2 type
    Uses a FretConfig interface to sets the number of frets for a GuitarNote2 instance.
    
    + Adds GuitarState enum to track strum, hopo, and tap state.

---
+++ YARG.Core/NewParsing/Notes/Guitar/IFretConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public interface IFretConfig
+    {
+        public int NumColors { get; }
+    }
+
+    public struct FiveFret : IFretConfig
+    {
+        public DualTime Open;
+        public DualTime Green;
+        public DualTime Red;
+        public DualTime Yellow;
+        public DualTime Blue;
+        public DualTime Orange;
+
+        public readonly int NumColors => 5;
+    }
+
+    public struct SixFret : IFretConfig
+    {
+        public DualTime Open;
+        public DualTime Black1;
+        public DualTime Black2;
+        public DualTime Black3;
+        public DualTime White1;
+        public DualTime White2;
+        public DualTime White3;
+
+        public readonly int NumColors => 6;
+    }
+}
