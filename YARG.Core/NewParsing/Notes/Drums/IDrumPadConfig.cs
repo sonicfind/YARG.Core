66d0728d1a77e84c98d54964a554b08b0f262a9b
sonicfind <sonicfind2@gmail.com>
2024-02-22 10:04:14 -0600

    Implement generic DrumNote2 & ProDrumNote2 types
    Backed by a IDrumNote interface, both types use a TDrumPadConfig generic to specify the number pads in an instance. Under the hood, the ProDrums variant holds a variable of the base type with just the addition of an array of cymbals on top in a `fixed` configuration.

---
+++ YARG.Core/NewParsing/Notes/Drums/IDrumPadConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public interface IDrumPadConfig
+    {
+        public int NumPads { get; }
+    }
+
+    public struct FourLane : IDrumPadConfig
+    {
+        public DrumPad Snare;
+        public DrumPad Yellow;
+        public DrumPad Blue;
+        public DrumPad Green;
+
+        public readonly int NumPads => 4;
+    }
+
+    public struct FiveLane : IDrumPadConfig
+    {
+        public DrumPad Snare;
+        public DrumPad Yellow;
+        public DrumPad Blue;
+        public DrumPad Orange;
+        public DrumPad Green;
+
+        public readonly int NumPads => 5;
+    }
+}
