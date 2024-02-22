2e99caee8fd5f88286bb5597146d5ff5627ca465
sonicfind <sonicfind2@gmail.com>
2024-02-22 10:04:14 -0600

    Implement generic DrumNote2 & ProDrumNote2 types
    Backed by a IDrumNote interface, both types use a TDrumPadConfig generic to specify the number pads in an instance. Under the hood, the ProDrums variant holds a variable of the base type with just the addition of an array of cymbals on top in a `fixed` configuration.

---
+++ YARG.Core/NewParsing/Notes/Drums/DrumPad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public enum DrumDynamics
+    {
+        None,
+        Accent,
+        Ghost
+    }
+
+    public struct DrumPad
+    {
+        public DualTime Duration;
+        public DrumDynamics Dynamics;
+
+        public readonly bool IsActive()
+        {
+            return Duration.IsActive();
+        }
+    }
+}
