7b98b2ab1cddc4464772b4426063c49162e0d167
sonicfind <sonicfind2@gmail.com>
2024-02-23 10:37:27 -0600

    Implement ProKeys note structures
    Allows up to four keys to be hit at a time, adding keys with stack-like behavior.

---
+++ YARG.Core/NewParsing/Notes/ProKeys/PitchedKey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public struct PitchedKey
+    {
+        public struct ProKeyConfig : IPitchConfig
+        {
+            public readonly int OCTAVE_MIN => 3;
+            public readonly int OCTAVE_MAX => 5;
+        }
+
+        public Pitch<ProKeyConfig> Pitch;
+        public DualTime Duration;
+    }
+}
