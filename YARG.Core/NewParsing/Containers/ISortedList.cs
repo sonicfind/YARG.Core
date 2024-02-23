e0754115201bf2db4de516ef5ed306f9ddd6964e
sonicfind <sonicfind2@gmail.com>
2024-02-23 00:22:02 -0600

    Implement necessary SortedList containers
    These are required for a few reasons:
    1. We'll need to return values from lists by reference if we want to be able to edit values of struct types used as TValue. List<T> & Dictionary do not provide that kind of access.
    2. We'll need a combination of the ability to append to end of the list (with ordering provided by properly formatted chart information) and the ability to insert through binary search (important for some special phrases).
    3. Allows us to add an optimized native version that makes use of explicit unmanaged structs and fixed memory for speed goodness (without exposing those mechanics for outside manipulation).

---
+++ YARG.Core/NewParsing/Containers/ISortedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public interface ISortedList<TKey, TValue> : IEnumerable<YARGKeyValuePair<TKey, TValue>>
+        where TKey : IEquatable<TKey>, IComparable<TKey>
+    {
+        protected const int DEFAULT_CAPACITY = 16;
+
+        public int Count { get; }
+        public int Capacity { get; set; }
+
+        public Span<YARGKeyValuePair<TKey, TValue>> Span { get; }
+
+        public void Clear();
+
+        public ref TValue Append(TKey key);
+
+        public ref TValue Append(TKey key, TValue value);
+
+        public ref TValue Append(TKey key, in TValue value);
+
+        public ref TValue this[TKey key] => ref FindOrEmplaceValue(0, key);
+
+        public int FindOrEmplaceIndex(int startIndex, TKey key);
+
+        public ref TValue FindOrEmplaceValue(int startIndex, TKey key);
+
+        /// <remarks>
+        /// Does not check for correct key ordering on forced insertion. Unsafe.
+        /// </remarks>
+        public void Insert_Forced(int index, TKey key, in TValue value);
+
+        public void Pop();
+
+        public ref TValue At(TKey key);
+
+        public ref YARGKeyValuePair<TKey, TValue> ElementAtIndex(int index);
+
+        public ref TValue GetLastOrAppend(TKey key);
+
+        public int Find(int startIndex, TKey key);
+
+        public bool ValidateLastKey(TKey key);
+
+        public ref TValue Last();
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!Try_Add(key, in value))
+            {
+                throw new ArgumentException($"A value of key of value {key} already exists");
+            }
+        }
+
+        public void Add(TKey key, in TValue value)
+        {
+            if (!Try_Add(key, in value))
+            {
+                throw new ArgumentException($"A value of key of value {key} already exists");
+            }
+        }
+
+        public bool Try_Add(TKey key, TValue value)
+        {
+            return Try_Add(key, in value);
+        }
+
+        public bool Try_Add(TKey key, in TValue value)
+        {
+            int index = Find(0, key);
+            if (index >= 0)
+            {
+                return false;
+            }
+
+            index = ~index;
+            Insert_Forced(index, key, in value);
+            return true;
+        }
+
+        /// <remarks>
+        /// Does not check for correct key ordering on forced insertion. Unsafe.
+        /// </remarks>
+        public void Insert_Forced(int index, TKey key, TValue value)
+        {
+            Insert_Forced(index + 1, key, in value);
+        }
+
+        public bool TryGetValue(TKey key, out TValue? value);
+
+        public bool ContainsKey(TKey key) { return ContainsKey(0, key); }
+
+        public bool ContainsKey(int startIndex, TKey key) { return Find(startIndex, key) >= 0; }
+    }
+}
---
+++ YARG.Core/NewParsing/Containers/YARGNativeSortedList.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public sealed unsafe class YARGNativeSortedList<TKey, TValue> : ISortedList<TKey, TValue>, IDisposable
+        where TKey : unmanaged, IEquatable<TKey>, IComparable<TKey>
+        where TValue : unmanaged
+    {
+        private static readonly int SIZEOF_PAIR = sizeof(YARGKeyValuePair<TKey, TValue>);
+
+        private YARGKeyValuePair<TKey, TValue>* _buffer = null;
+        private int _capacity;
+        private int _count;
+        private int _version;
+        private bool _disposed;
+
+        public int Count => _count;
+
+        public int Capacity
+        {
+            get => _capacity;
+            set
+            {
+                if (_disposed)
+                {
+                    throw new ObjectDisposedException(GetType().Name);
+                }
+
+                if (_count <= value && value != _capacity)
+                {
+                    if (value > 0)
+                    {
+                        int size = value * SIZEOF_PAIR;
+                        if (_buffer != null)
+                        {
+                            _buffer = (YARGKeyValuePair<TKey, TValue>*) Marshal.ReAllocHGlobal((IntPtr) _buffer, (IntPtr)size);
+                        }
+                        else
+                        {
+                            _buffer = (YARGKeyValuePair<TKey, TValue>*) Marshal.AllocHGlobal(size);
+                        }
+                        ++_version;
+                    }
+                    else
+                    {
+                        if (_buffer != null)
+                        {
+                            Marshal.FreeHGlobal((IntPtr) _buffer);
+                        }
+                        _buffer = null;
+                        _version = 0;
+                    }
+                    _capacity = value;
+                }
+            }
+        }
+
+        public Span<YARGKeyValuePair<TKey, TValue>> Span => new (_buffer, _count);
+
+        public YARGNativeSortedList() { }
+
+        public YARGNativeSortedList(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public void Clear()
+        {
+            if (_count > 0)
+            {
+                _version++;
+            }
+            _count = 0;
+        }
+
+        private void Dispose(bool _)
+        {
+            if (!_disposed)
+            {
+                if (_buffer != null)
+                {
+                    Marshal.FreeHGlobal((IntPtr) _buffer);
+                }
+                _buffer = null;
+                _version = 0;
+                _capacity = 0;
+                _count = 0;
+                _disposed = true;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~YARGNativeSortedList()
+        {
+            Dispose(false);
+        }
+
+        public ref TValue Append(TKey key)
+        {
+            CheckAndGrow();
+            int index = _count++;
+
+            var node = _buffer + index;
+            node->Key = key;
+            node->Value = default;
+            return ref node->Value;
+        }
+
+        public ref TValue Append(TKey key, TValue value)
+        {
+            CheckAndGrow();
+            int index = _count++;
+
+            var node = _buffer + index;
+            node->Key = key;
+            node->Value = value;
+            return ref node->Value;
+        }
+
+        public ref TValue Append(TKey key, in TValue value)
+        {
+            CheckAndGrow();
+            int index = _count++;
+
+            var node = _buffer + index;
+            node->Key = key;
+            node->Value = value;
+            return ref node->Value;
+        }
+
+        public ref TValue this[TKey key] => ref FindOrEmplaceValue(0, key);
+
+        public int FindOrEmplaceIndex(int startIndex, TKey key)
+        {
+            int index = Find(startIndex, key);
+            if (index < 0)
+            {
+                index = ~index;
+                Insert_Forced(index, key, new());
+            }
+            return index;
+        }
+
+        public ref TValue FindOrEmplaceValue(int startIndex, TKey key)
+        {
+            int index = FindOrEmplaceIndex(startIndex, key);
+            return ref _buffer[index].Value;
+        }
+
+        /// <remarks>
+        /// Does not check for correct key ordering on forced insertion. Unsafe.
+        /// </remarks>
+        public void Insert_Forced(int index, TKey key, in TValue value)
+        {
+            CheckAndGrow();
+            var position = _buffer + index;
+            if (index < _count)
+            {
+                int leftover = _count - index;
+                Buffer.MemoryCopy(position, position + 1, leftover, leftover);
+            }
+
+            ++_count;
+            position->Key = key;
+            position->Value = value;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            int index = Find(0, key);
+            if (index < 0)
+            {
+                value = default!;
+                return false;
+            }
+            value = _buffer[index].Value;
+            return true;
+        }
+
+        public void Pop()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            --_count;
+            ++_version;
+        }
+
+        public ref TValue At(TKey key)
+        {
+            int index = Find(0, key);
+            if (index < 0)
+            {
+                throw new KeyNotFoundException();
+            }
+            return ref _buffer[index].Value;
+        }
+
+        public ref YARGKeyValuePair<TKey, TValue> ElementAtIndex(int index)
+        {
+            if (index < 0 || _count <= index)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return ref _buffer[index];
+        }
+
+        public ref TValue GetLastOrAppend(TKey key)
+        {
+            if (_count == 0 || _buffer[_count - 1] < key)
+            {
+                return ref Append(key);
+            }
+            return ref _buffer[_count - 1].Value;
+        }
+
+        public int Find(int startIndex, TKey key)
+        {
+            var lo = _buffer + startIndex;
+            var hi = _buffer + Count - (startIndex + 1);
+            while (lo <= hi)
+            {
+                var curr = lo + ((hi - lo) >> 1);
+                int order = curr->CompareTo(key);
+                if (order == 0)
+                {
+                    return (int)(curr - _buffer);
+                }
+
+                if (order < 0)
+                {
+                    lo = curr + 1;
+                }
+                else
+                {
+                    hi = curr - 1;
+                }
+            }
+            return ~(int)(lo - _buffer);
+        }
+
+        public bool ValidateLastKey(TKey key)
+        {
+            return _count > 0 && _buffer[_count - 1].Equals(key);
+        }
+
+        public ref TValue Last() { return ref _buffer[_count - 1].Value; }
+
+        private void CheckAndGrow()
+        {
+            if (_count >= int.MaxValue)
+            {
+                throw new OverflowException("Element limit reached");
+            }
+
+            if (_count == _capacity)
+            {
+                Grow();
+            }
+            ++_version;
+        }
+
+        private void Grow()
+        {
+            int newcapacity = _capacity == 0 ? ISortedList<TKey, TValue>.DEFAULT_CAPACITY : 2 * _capacity;
+            if ((uint) newcapacity > int.MaxValue)
+            {
+                newcapacity = int.MaxValue;
+            }
+            Capacity = newcapacity;
+        }
+
+        public IEnumerator GetEnumerator() { return new Enumerator(this); }
+
+        IEnumerator<YARGKeyValuePair<TKey, TValue>> IEnumerable<YARGKeyValuePair<TKey, TValue>>.GetEnumerator()
+        {
+            return ((IEnumerable<YARGKeyValuePair<TKey, TValue>>) this).GetEnumerator();
+        }
+
+        public struct Enumerator : IEnumerator<YARGKeyValuePair<TKey, TValue>>, IEnumerator
+        {
+            private readonly YARGNativeSortedList<TKey, TValue> _map;
+            private readonly YARGKeyValuePair<TKey, TValue>* _end;
+            private readonly int _version;
+            
+            private YARGKeyValuePair<TKey, TValue>* _current;
+
+            internal Enumerator(YARGNativeSortedList<TKey, TValue> map)
+            {
+                _map = map;
+                _end = map._buffer != null ? map._buffer + map.Count : null;
+                _version = map._version;
+                _current = null;
+            }
+
+            public readonly void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if (_version != _map._version)
+                {
+                    throw new InvalidOperationException("Enum failed - Sorted List was updated");
+                }
+
+                if (_end == null)
+                {
+                    return false;
+                }
+
+                if (_current != null)
+                {
+                    ++_current;
+                }
+                else
+                {
+                    _current = _map._buffer;
+                }
+                return _current < _end;
+            }
+
+            public readonly YARGKeyValuePair<TKey, TValue> Current
+            {
+                get
+                {
+                    if (_version != _map._version || _current == null || _current >= _end)
+                    {
+                        throw new InvalidOperationException("Enum Operation not possible");
+                    }
+                    return *_current;
+                }
+            }
+
+            readonly object IEnumerator.Current => Current;
+
+            void IEnumerator.Reset()
+            {
+                if (_version != _map._version)
+                {
+                    throw new InvalidOperationException("Enum failed - Sorted List was updated");
+                }
+                _current = null;
+            }
+        }
+    }
+}
