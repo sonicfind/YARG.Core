d2a4f41cd6969d62f109c4b5d631a56421f40bd9
sonicfind <sonicfind2@gmail.com>
2024-02-23 12:25:33 -0600

    Add basic structure for SyncTrack

---
+++ YARG.Core/NewParsing/Sync/SyncTrack2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YARG.Core.Chart;
+
+namespace YARG.Core.NewParsing
+{
+    public class SyncTrack2 : IDisposable
+    {
+        public uint Tickrate;
+        public readonly YARGNativeSortedList<long, Tempo2> TempoMarkers = new();
+        public readonly YARGNativeSortedList<long, TimeSig2> TimeSigs = new();
+        public readonly YARGNativeSortedList<DualTime, BeatlineType> BeatMap = new();
+
+        public void Dispose()
+        {
+            TempoMarkers.Dispose();
+            TimeSigs.Dispose();
+            BeatMap.Dispose();
+        }
+    }
+}
