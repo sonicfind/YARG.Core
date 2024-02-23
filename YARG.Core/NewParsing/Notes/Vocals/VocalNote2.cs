66cc2e30c28064e9ae1168600c1dedb8549936ed
sonicfind <sonicfind2@gmail.com>
2024-02-23 13:18:50 -0600

    Add VocalNote & Vocal Percussion structures

---
+++ YARG.Core/NewParsing/Notes/Vocals/VocalNote2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public struct VocalNote2
+    {
+        public struct VocalConfig : IPitchConfig
+        {
+            public readonly int OCTAVE_MIN => 2;
+            public readonly int OCTAVE_MAX => 6;
+        }
+
+        public string Lyric;
+        public Pitch<VocalConfig> Pitch;
+        public DualTime Duration;
+
+        public readonly bool IsPlayable() { return Lyric.Length > 0 && (Pitch.Octave >= 2 || Lyric[0] == '#'); }
+    }
+}
