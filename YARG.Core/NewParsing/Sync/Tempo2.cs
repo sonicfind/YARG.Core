8e7e1e564dc5dfb54720e32b7e8a1de3e0b478da
sonicfind <sonicfind2@gmail.com>
2024-02-23 01:10:12 -0600

    Add basic TimeSig & Tempo structs

---
+++ YARG.Core/NewParsing/Sync/Tempo2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public struct Tempo2
+    {
+        public const int BPM_FACTOR = 60000000;
+        public const int DEFAULT_BPM = 120;
+        public const int MICROS_AT_120BPM = BPM_FACTOR / DEFAULT_BPM;
+
+        public static readonly Tempo2 DEFAULT = new()
+        {
+            Micros = MICROS_AT_120BPM
+        };
+
+        public int Micros;
+        public long Anchor;
+
+        public float BPM
+        {
+            readonly get { return Micros != 0 ? (float) BPM_FACTOR / Micros : 0; }
+            set { Micros = value != 0 ? (int) (BPM_FACTOR / value) : 0; }
+        }
+    }
+}
