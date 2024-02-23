a6826e736e8af1d5aee1f16f5397f3e75170749e
sonicfind <sonicfind2@gmail.com>
2024-02-23 13:18:50 -0600

    Add VocalNote & Vocal Percussion structures

---
+++ YARG.Core/NewParsing/Notes/Vocals/VocalPercussion2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public struct VocalPercussion2
+    {
+        public bool IsPlayable;
+    }
+}
