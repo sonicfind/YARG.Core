a3cefcf0a10f4f2bb7d27a64ebfd4fbd903cfc48
sonicfind <sonicfind2@gmail.com>
2024-02-23 01:22:30 -0600

    Add basic InstrumnentTrack & DifficultyTrack classes

---
+++ YARG.Core/NewParsing/Tracks/Instruments/DifficultyTrack2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public class DifficultyTrack2<TNote> : Track
+        where TNote : unmanaged, IInstrumentNote
+    {
+        public readonly YARGNativeSortedList<DualTime, TNote> Notes = new();
+
+        public DifficultyTrack2() { }
+        public DifficultyTrack2(int capcacity)
+        {
+            Notes.Capacity = capcacity;
+        }
+
+        public override bool IsOccupied() { return !Notes.IsEmpty() || base.IsOccupied(); }
+
+        public override void Clear()
+        {
+            base.Clear();
+            Notes.Clear();
+        }
+
+        public override void TrimExcess() => Notes.TrimExcess();
+
+        public override DualTime GetLastNoteTime()
+        {
+            if (Notes.IsEmpty())
+            {
+                return default;
+            }
+
+            var note = Notes.ElementAtIndex(Notes.Count - 1);
+            return note.Key + note.Value.GetLongestSustain();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Notes.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+        }
+    }
+}
