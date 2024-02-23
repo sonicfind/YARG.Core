1a0be7f6f8b355c8734981ae1377e3b58f1198b6
sonicfind <sonicfind2@gmail.com>
2024-02-23 01:22:30 -0600

    Add basic InstrumnentTrack & DifficultyTrack classes

---
+++ YARG.Core/NewParsing/Tracks/Instruments/BasicInstrumentTrack.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public class BasicInstrumentTrack<TNote> : InstrumentTrack2<DifficultyTrack2<TNote>>
+        where TNote : unmanaged, IInstrumentNote
+    {
+    }
+}
