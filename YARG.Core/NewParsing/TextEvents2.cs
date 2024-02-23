6c480288d45676ac2102f921e4ad579fcb953deb
sonicfind <sonicfind2@gmail.com>
2024-02-23 14:13:43 -0600

    Add structures to handle & hold text events
    SongSection functions as a wrapper for a non-nullable string. This was necessary to supply a default constructor - which YARGManagedSortedList requires.

---
+++ YARG.Core/NewParsing/TextEvents2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public class TextEvents2 : IDisposable
+    {
+        public readonly YARGManagedSortedList<DualTime, SongSection2> Sections = new();
+        public readonly YARGManagedSortedList<DualTime, List<string>> Globals = new();
+
+        public void Clear()
+        {
+            Sections.Clear();
+            Globals.Clear();
+        }
+
+        public void Dispose()
+        {
+            Sections.Dispose();
+            Globals.Dispose();
+        }
+    }
+}
