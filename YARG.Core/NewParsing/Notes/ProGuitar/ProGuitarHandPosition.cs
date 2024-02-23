4353cccc4b039d21d800bee4455355ec14512d1a
sonicfind <sonicfind2@gmail.com>
2024-02-23 09:25:02 -0600

    Add ProGuitarNote structures

---
+++ YARG.Core/NewParsing/Notes/ProGuitar/ProGuitarHandPosition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public struct HandPosition<TFretConfig>
+        where TFretConfig : unmanaged, IProFretConfig
+    {
+        private static readonly TFretConfig CONFIG = default;
+        private int _fret;
+
+        public int Fret
+        {
+            readonly get => _fret;
+            set
+            {
+                _fret = CONFIG.ValidateFret(value);
+            }
+        }
+    }
+}
