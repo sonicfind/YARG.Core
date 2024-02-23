9b15c763b1394e59f7a25ebb9c4e3c2cdd0a7b96
sonicfind <sonicfind2@gmail.com>
2024-02-23 00:22:02 -0600

    Implement necessary SortedList containers
    These are required for a few reasons:
    1. We'll need to return values from lists by reference if we want to be able to edit values of struct types used as TValue. List<T> & Dictionary do not provide that kind of access.
    2. We'll need a combination of the ability to append to end of the list (with ordering provided by properly formatted chart information) and the ability to insert through binary search (important for some special phrases).
    3. Allows us to add an optimized native version that makes use of explicit unmanaged structs and fixed memory for speed goodness (without exposing those mechanics for outside manipulation).

---
+++ YARG.Core/NewParsing/Containers/YARGKeyValuePair.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    [DebuggerDisplay("{key} | {Value.ToString()}")]
+    public struct YARGKeyValuePair<TKey, TValue> : IComparable<TKey>, IEquatable<TKey>
+        where TKey : IEquatable<TKey>, IComparable<TKey>
+    {
+        public TKey Key;
+        public TValue Value;
+
+        public int CompareTo(TKey key)
+        {
+            return Key.CompareTo(key);
+        }
+
+        public bool Equals(TKey key)
+        {
+            return Key.Equals(key);
+        }
+
+        public static bool operator <(YARGKeyValuePair<TKey, TValue> node, TKey key) { return node.Key.CompareTo(key) < 0; }
+        public static bool operator >(YARGKeyValuePair<TKey, TValue> node, TKey key) { return node.Key.CompareTo(key) > 0; }
+    }
+}
