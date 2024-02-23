b6efeae0228fea16f650669395f0dbaac45fa3d3
sonicfind <sonicfind2@gmail.com>
2024-02-23 12:46:49 -0600

    Convert ProGuitar fret validation to a static function of the base interface
    Alters the interface to hold a static instance of the derived type that provides the fret limit.

--- YARG.Core/NewParsing/Notes/ProGuitar/IProFretConfig.cs
+++ YARG.Core/NewParsing/Notes/ProGuitar/IProFretConfig.cs
@@ -4,34 +4,29 @@ using System.Text;
 
 namespace YARG.Core.NewParsing
 {
-    public interface IProFretConfig
+    public interface IProFretConfig<TConfig>
+        where TConfig : unmanaged, IProFretConfig<TConfig>
     {
-        public int ValidateFret(int fret);
-    }
-
-    public struct ProFret_17 : IProFretConfig
-    {
-        private const int MAX_FRET = 17;
-        public readonly int ValidateFret(int fret)
+        private static readonly TConfig CONFIG = default;
+        public static int ValidateFret(int fret)
         {
-            if (fret < 0 || MAX_FRET < fret)
+            if (fret < 0 || CONFIG.MAX_FRET < fret)
             {
-                throw new ArgumentOutOfRangeException($"Fret value must lie in the range of [0, {MAX_FRET}]");
+                throw new ArgumentOutOfRangeException($"Fret value must lie in the range of [0, {CONFIG.MAX_FRET}]");
             }
             return fret;
         }
+
+        public int MAX_FRET { get; }
     }
 
-    public struct ProFret_22 : IProFretConfig
+    public struct ProFret_17 : IProFretConfig<ProFret_17>
     {
-        private const int MAX_FRET = 22;
-        public readonly int ValidateFret(int fret)
-        {
-            if (fret < 0 || MAX_FRET < fret)
-            {
-                throw new ArgumentOutOfRangeException($"Fret value must lie in the range of [0, {MAX_FRET}]");
-            }
-            return fret;
-        }
+        public readonly int MAX_FRET => 17;
+    }
+
+    public struct ProFret_22 : IProFretConfig<ProFret_22>
+    {
+        public readonly int MAX_FRET => 22;
     }
 }
