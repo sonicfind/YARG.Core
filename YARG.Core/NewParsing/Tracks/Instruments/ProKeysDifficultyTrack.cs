39b593a4e51cc678cac8b44140be03c6ce85f1c3
sonicfind <sonicfind2@gmail.com>
2024-02-23 13:08:27 -0600

    Add specialize ProKeys difficulty track

---
+++ YARG.Core/NewParsing/Tracks/Instruments/ProKeysDifficultyTrack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public enum ProKey_Ranges
+    {
+        C1_E2,
+        D1_F2,
+        E1_G2,
+        F1_A2,
+        G1_B2,
+        A1_C3,
+    };
+
+    public class ProKeysDifficultyTrack : DifficultyTrack2<ProKeyNote>
+    {
+        public readonly YARGNativeSortedList<DualTime, ProKey_Ranges> Ranges = new();
+
+        public override bool IsOccupied() { return !Ranges.IsEmpty() || base.IsOccupied(); }
+        public override void Clear()
+        {
+            
+            Ranges.Clear();
+            base.Clear();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Ranges.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+        }
+    }
+}
