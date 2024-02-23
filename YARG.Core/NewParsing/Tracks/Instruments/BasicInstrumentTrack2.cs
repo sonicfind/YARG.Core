74c9e6e7241a8874a3c1e1ddac177e21e838bed5
sonicfind <sonicfind2@gmail.com>
2024-02-23 13:14:20 -0600

    Add "2" suffix to BasicInstrumentTrack

--- YARG.Core/NewParsing/Tracks/Instruments/BasicInstrumentTrack2.cs
+++ YARG.Core/NewParsing/Tracks/Instruments/BasicInstrumentTrack2.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace YARG.Core.NewParsing
 {
-    public class BasicInstrumentTrack<TNote> : InstrumentTrack2<DifficultyTrack2<TNote>>
+    public class BasicInstrumentTrack2<TNote> : InstrumentTrack2<DifficultyTrack2<TNote>>
         where TNote : unmanaged, IInstrumentNote
     {
     }
