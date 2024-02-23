38d831faa77608a8dafa491e4736f486c975ee28
sonicfind <sonicfind2@gmail.com>
2024-02-23 12:56:59 -0600

    Add specialized ProGuitar difficulty and instrument tracks

---
+++ YARG.Core/NewParsing/Tracks/Instruments/ProGuitarDifficultyTrack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public class ProGuitarDifficultyTrack<TProFretConfig> : DifficultyTrack2<ProGuitarNote<TProFretConfig>>
+        where TProFretConfig : unmanaged, IProFretConfig<TProFretConfig>
+    {
+        public readonly YARGNativeSortedList<DualTime, Arpeggio<TProFretConfig>> Arpeggios = new();
+
+        public override bool IsOccupied() { return !Arpeggios.IsEmpty() || base.IsOccupied(); }
+
+        public override void Clear()
+        {
+            Arpeggios.Clear();
+            base.Clear();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Arpeggios.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+        }
+    }
+}
---
+++ YARG.Core/NewParsing/Tracks/Instruments/ProGuitarInstrumentTrack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public enum ChordPhrase
+    {
+        Force_Numbering,
+        Slash,
+        Hide,
+        Accidental_Switch
+    };
+
+    public class ProGuitarInstrumentTrack<TProFretConfig> : InstrumentTrack2<ProGuitarDifficultyTrack<TProFretConfig>>, IDisposable
+        where TProFretConfig : unmanaged, IProFretConfig<TProFretConfig>
+    {
+        public readonly YARGNativeSortedList<DualTime, PitchName> Roots = new();
+        public readonly YARGNativeSortedList<DualTime, HandPosition<TProFretConfig>> HandPositions = new();
+        public readonly YARGManagedSortedList<DualTime, List<ChordPhrase>> ChordPhrases = new();
+
+        public override bool IsOccupied()
+        {
+            return !Roots.IsEmpty() || !HandPositions.IsEmpty() || !ChordPhrases.IsEmpty() || base.IsOccupied();
+        }
+
+        public override void Clear()
+        {
+            Roots.Clear();
+            HandPositions.Clear();
+            ChordPhrases.Clear();
+            base.Clear();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Roots.Dispose();
+                    HandPositions.Dispose();
+                    ChordPhrases.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+        }
+    }
+}
