7f2d2e15c003d297c177ca6ebce98f335df4bc62
sonicfind <sonicfind2@gmail.com>
2024-02-24 00:21:55 -0600

    Add Tick <-> Seconds conversion functions to SyncTrack class

--- YARG.Core/NewParsing/Sync/SyncTrack2.cs
+++ YARG.Core/NewParsing/Sync/SyncTrack2.cs
@@ -12,6 +12,58 @@ namespace YARG.Core.NewParsing
         public readonly YARGNativeSortedList<long, TimeSig2> TimeSigs = new();
         public readonly YARGNativeSortedList<DualTime, BeatlineType> BeatMap = new();
 
+        public double ConvertToSeconds(long ticks, int startIndex = 0)
+        {
+            return ConvertToSeconds(ticks, ref startIndex);
+        }
+
+        public double ConvertToSeconds(long ticks, ref int startIndex)
+        {
+            unsafe
+            {
+                var curr = TempoMarkers.Data + startIndex;
+                var end = TempoMarkers.End;
+                while (curr < end)
+                {
+                    if (curr + 1 == end || ticks < curr[1].Key)
+                    {
+                        startIndex = (int)(curr - TempoMarkers.Data);
+
+                        double quarters = (ticks - curr->Key) / (double) Tickrate;
+                        long micros = (long)(curr->Value.MicrosPerQuarter * quarters) + curr->Value.Anchor;
+                        return micros / (double) Tempo2.MICROS_PER_SECOND;
+                    }
+                }
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+        }
+
+        public long ConvertToTicks(double seconds, int startIndex = 0)
+        {
+            return ConvertToTicks(seconds, ref startIndex);
+        }
+
+        public long ConvertToTicks(double seconds, ref int startIndex)
+        {
+            long micros = (long) (seconds * Tempo2.MICROS_PER_SECOND);
+            unsafe
+            {
+                var curr = TempoMarkers.Data + startIndex;
+                var end = TempoMarkers.End;
+                while (curr < end)
+                {
+                    if (curr + 1 == end || micros < curr[1].Key)
+                    {
+                        startIndex = (int) (curr - TempoMarkers.Data);
+
+                        double quarters = (micros - curr->Value.Anchor) / (double) curr->Value.MicrosPerQuarter;
+                        return (long)(quarters * Tickrate) + curr->Key;
+                    }
+                }
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+        }
+
         public void Dispose()
         {
             TempoMarkers.Dispose();
