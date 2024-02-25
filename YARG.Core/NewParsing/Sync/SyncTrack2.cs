28eac26952685c5cf7fd7b5073cc11abafbcdb38
sonicfind <sonicfind2@gmail.com>
2024-02-24 22:31:14 -0600

    Add SyncTrack constructor that receives the tickrate
    + Inserts a starting default tempo & timesig (and thus removes the checks in the anchor finalization function)

--- YARG.Core/NewParsing/Sync/SyncTrack2.cs
+++ YARG.Core/NewParsing/Sync/SyncTrack2.cs
@@ -11,6 +11,13 @@ namespace YARG.Core.NewParsing
         public readonly YARGNativeSortedList<long, Tempo2> TempoMarkers = new();
         public readonly YARGNativeSortedList<long, TimeSig2> TimeSigs = new();
 
+        public SyncTrack2(uint tickrate)
+        {
+            _tickrate = tickrate;
+            TempoMarkers.Append(0, Tempo2.DEFAULT);
+            TimeSigs.Append(0, TimeSig2.DEFAULT);
+        }
+
         public double ConvertToSeconds(long ticks, int startIndex = 0)
         {
             return ConvertToSeconds(ticks, ref startIndex);
@@ -69,28 +76,18 @@ namespace YARG.Core.NewParsing
             TimeSigs.Dispose();
         }
 
-        public static void Finalize(SyncTrack2 sync)
+        private void FinalizeAnchors()
         {
-            if (sync.TempoMarkers.IsEmpty() || sync.TempoMarkers.ElementAtIndex(0).Key != 0)
-            {
-                sync.TempoMarkers.Insert_Forced(0, 0, Tempo2.DEFAULT);
-            }
-
-            if (sync.TimeSigs.IsEmpty() || sync.TimeSigs.ElementAtIndex(0).Key != 0)
-            {
-                sync.TimeSigs.Insert_Forced(0, 0, TimeSig2.DEFAULT);
-            }
-
             unsafe
             {
-                var end = sync.TempoMarkers.End;
+                var end = TempoMarkers.End;
                 // We can skip the first Anchor, even if not explicitly set (as it'd still be 0)
-                for (var marker = sync.TempoMarkers.Data + 1; marker < end; ++marker)
+                for (var marker = TempoMarkers.Data + 1; marker < end; ++marker)
                 {
                     if (marker->Value.Anchor == 0)
                     {
                         var prev = marker - 1;
-                        marker->Value.Anchor = (long) (((marker->Key - prev->Key) / (float) sync._tickrate) * prev->Value.MicrosPerQuarter) + prev->Value.Anchor;
+                        marker->Value.Anchor = (long) (((marker->Key - prev->Key) / (float) _tickrate) * prev->Value.MicrosPerQuarter) + prev->Value.Anchor;
                     }
                 }
             }
