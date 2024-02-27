0ac470c7216c6285ea82d3a54f58c22169b1cae9
sonicfind <sonicfind2@gmail.com>
2024-02-26 23:33:33 -0600

    Remove unnecessary `using` declarations for MidiLoaders

--- YARG.Core/NewParsing/MidiLoader/Guitar/GuitarMidiDifficulty.cs
+++ YARG.Core/NewParsing/MidiLoader/Guitar/GuitarMidiDifficulty.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace YARG.Core.NewParsing.Midi
+﻿namespace YARG.Core.NewParsing.Midi
 {
     public struct GuitarMidiDifficulty
     {
