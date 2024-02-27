42a4fb35b0ccee1e4dd194c2429f4733e02e3e5c
sonicfind <sonicfind2@gmail.com>
2024-02-27 11:53:19 -0600

    Implement .mid loader functionality

--- YARG.Core/NewParsing/YARGChart.cs
+++ YARG.Core/NewParsing/YARGChart.cs
@@ -47,11 +47,12 @@ namespace YARG.Core.NewParsing
         public VocalTrack2? LeadVocals;
         public VocalTrack2? HarmonyVocals;
 
-        public YARGChart(SyncTrack2 sync, SongMetadata metadata, ParseSettings settings)
+        public YARGChart(SyncTrack2 sync, SongMetadata metadata, ParseSettings settings, string? midiSequenceName = null)
         {
             Sync = sync;
             Metadata = metadata;
             Settings = settings;
+            MidiSequenceName = midiSequenceName ?? string.Empty;
         }
 
         public void Dispose()
