1326101f6b2e4715139c1564a23369dbe0bdfb25
sonicfind <sonicfind2@gmail.com>
2024-03-03 15:23:32 -0600

    Add bunch of ToString() & Debugger items

--- YARG.Core/NewParsing/Notes/Guitar/IFretConfig.cs
+++ YARG.Core/NewParsing/Notes/Guitar/IFretConfig.cs
@@ -19,6 +19,24 @@ namespace YARG.Core.NewParsing
         public DualTime Orange;
 
         public readonly int NumColors => 5;
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new();
+            if (Open.IsActive())
+                stringBuilder.Append($"Open: {Open.Ticks} | ");
+            if (Green.IsActive())
+                stringBuilder.Append($"Green: {Green.Ticks} | ");
+            if (Red.IsActive())
+                stringBuilder.Append($"Red: {Red.Ticks} | ");
+            if (Yellow.IsActive())
+                stringBuilder.Append($"Yellow: {Yellow.Ticks} | ");
+            if (Blue.IsActive())
+                stringBuilder.Append($"Blue: {Blue.Ticks} | ");
+            if (Orange.IsActive())
+                stringBuilder.Append($"Orange: {Orange.Ticks}");
+            return stringBuilder.ToString();
+        }
     }
 
     public struct SixFret : IFretConfig
@@ -32,5 +50,25 @@ namespace YARG.Core.NewParsing
         public DualTime White3;
 
         public readonly int NumColors => 6;
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new();
+            if (Open.IsActive())
+                stringBuilder.Append($"Open: {Open.Ticks} | ");
+            if (Black1.IsActive())
+                stringBuilder.Append($"Black 1: {Black1.Ticks} | ");
+            if (Black2.IsActive())
+                stringBuilder.Append($"Black 2: {Black2.Ticks} | ");
+            if (Black3.IsActive())
+                stringBuilder.Append($"Black 3: {Black3.Ticks} | ");
+            if (White1.IsActive())
+                stringBuilder.Append($"White 1: {White1.Ticks} | ");
+            if (White2.IsActive())
+                stringBuilder.Append($"White 2: {White2.Ticks} | ");
+            if (White3.IsActive())
+                stringBuilder.Append($"White 3: {White3.Ticks}");
+            return stringBuilder.ToString();
+        }
     }
 }
