67f27fa63004342ee1eae2c5204182f68fc6a82a
sonicfind <sonicfind2@gmail.com>
2024-03-06 14:00:06 -0600

    Move GuitarNote indexer to the FretConfig interface

--- YARG.Core/NewParsing/Notes/Guitar/IFretConfig.cs
+++ YARG.Core/NewParsing/Notes/Guitar/IFretConfig.cs
@@ -7,6 +7,8 @@ namespace YARG.Core.NewParsing
     public interface IFretConfig
     {
         public int NumColors { get; }
+
+        public ref DualTime this[int lane] { get; }
     }
 
     public struct FiveFret : IFretConfig
@@ -19,6 +21,28 @@ namespace YARG.Core.NewParsing
         public DualTime Orange;
 
         public readonly int NumColors => 5;
+
+        public ref DualTime this[int lane]
+        {
+            get
+            {
+                unsafe
+                {
+#pragma warning disable CS9084 // Struct member returns 'this' or other instance members by reference
+                    switch (lane)
+                    {
+                        case 0: return ref Open;
+                        case 1: return ref Green;
+                        case 2: return ref Red;
+                        case 3: return ref Yellow;
+                        case 4: return ref Blue;
+                        case 5: return ref Orange;
+                        default: throw new ArgumentOutOfRangeException(nameof(lane));
+                    }
+#pragma warning restore CS9084 // Struct member returns 'this' or other instance members by reference
+                }
+            }
+        }
 
         public override string ToString()
         {
@@ -50,6 +74,29 @@ namespace YARG.Core.NewParsing
         public DualTime White3;
 
         public readonly int NumColors => 6;
+
+        public ref DualTime this[int lane]
+        {
+            get
+            {
+                unsafe
+                {
+#pragma warning disable CS9084 // Struct member returns 'this' or other instance members by reference
+                    switch (lane)
+                    {
+                        case 0: return ref Open;
+                        case 1: return ref Black1;
+                        case 2: return ref Black2;
+                        case 3: return ref Black3;
+                        case 4: return ref White1;
+                        case 5: return ref White2;
+                        case 6: return ref White3;
+                        default: throw new ArgumentOutOfRangeException(nameof(lane));
+                    }
+#pragma warning restore CS9084 // Struct member returns 'this' or other instance members by reference
+                }
+            }
+        }
 
         public override string ToString()
         {
