2935b89d77c6f88d111541dcfa20650f2e38039c
sonicfind <sonicfind2@gmail.com>
2024-03-05 21:18:59 -0600

    More "Try" getter functions for sortedlist classes
    The value type returned depends solely on the subtype of sortedlist. Native returns a pointer while Managed returns the bass type (which preferably should be a class).
    
    + Update Midi loader structures to utilize these new Try functions.
    + Remove YARGKeyValuePair's implementations of IComparable<TKey> & IEquatable<TKey>

--- YARG.Core/NewParsing/Containers/YARGKeyValuePair.cs
+++ YARG.Core/NewParsing/Containers/YARGKeyValuePair.cs
@@ -6,21 +6,11 @@ using System.Text;
 namespace YARG.Core.NewParsing
 {
     [DebuggerDisplay("{Key} = {Value}")]
-    public struct YARGKeyValuePair<TKey, TValue> : IComparable<TKey>, IEquatable<TKey>
+    public struct YARGKeyValuePair<TKey, TValue>
         where TKey : IEquatable<TKey>, IComparable<TKey>
     {
         public TKey Key;
         public TValue Value;
-
-        public int CompareTo(TKey key)
-        {
-            return Key.CompareTo(key);
-        }
-
-        public bool Equals(TKey key)
-        {
-            return Key.Equals(key);
-        }
 
         public static bool operator <(YARGKeyValuePair<TKey, TValue> node, TKey key) { return node.Key.CompareTo(key) < 0; }
         public static bool operator >(YARGKeyValuePair<TKey, TValue> node, TKey key) { return node.Key.CompareTo(key) > 0; }
