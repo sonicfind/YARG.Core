2df61a48626b573ec47c8b1c41aef5cb30d48228
sonicfind <sonicfind2@gmail.com>
2024-03-08 12:44:06 -0600

    Add VenueTrack2 type

---
+++ YARG.Core/NewParsing/Tracks/Venue/VenueEvent2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YARG.Core.Chart;
+
+namespace YARG.Core.NewParsing
+{
+    public struct VenueEvent2<TType>
+        where TType : unmanaged
+    {
+        private readonly VenueEventFlags _flags;
+        public TType Value;
+
+        public readonly bool IsOptional => (_flags & VenueEventFlags.Optional) != 0;
+
+        public VenueEvent2(TType value, VenueEventFlags flags = VenueEventFlags.None)
+        {
+            Value = value;
+            _flags = flags;
+        }
+    }
+}
---
+++ YARG.Core/NewParsing/Tracks/Venue/VenueTrack2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YARG.Core.Chart;
+
+namespace YARG.Core.NewParsing
+{
+    public class VenueTrack2 : IDisposable
+    {
+        public readonly YARGNativeSortedList<DualTime, VenueEvent2<LightingType>> Lighting = new();
+        public readonly YARGNativeSortedList<DualTime, VenueEvent2<PostProcessingType>> PostProcessing = new();
+        public readonly YARGNativeSortedList<DualTime, VenueEvent2<PerformerEvent2>> Performer = new();
+        public readonly YARGNativeSortedList<DualTime, VenueEvent2<StageEffect>> Stage = new();
+
+        public void Dispose()
+        {
+            Lighting.Dispose();
+            PostProcessing.Dispose();
+            Performer.Dispose();
+            Stage.Dispose();
+        }
+    }
+}
