1e77ffd4f32af13a47d4ac7b75bfc7e2780c0228
sonicfind <sonicfind2@gmail.com>
2024-03-08 12:44:06 -0600

    Add VenueTrack2 type

---
+++ YARG.Core/NewParsing/Tracks/Venue/PerformerEvent2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YARG.Core.Chart;
+
+namespace YARG.Core.NewParsing
+{
+    public struct PerformerEvent2
+    {
+        public PerformerEventType Type;
+        public Performer Performers;
+    }
+}
