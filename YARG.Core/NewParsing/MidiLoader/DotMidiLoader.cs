00e86e8ece6d2ea23c5d2267505b5ecd59f4443f
sonicfind <sonicfind2@gmail.com>
2024-03-09 16:31:00 -0600

    Address a missing `using` declaration in midi synctrack loading
    Wasn't a leak, per se, as the memory will get cleared up once the inner FixedArray buffer is finalized.
    However, benchmark.net noted this as a potential mistake (correctly so).

--- YARG.Core/NewParsing/MidiLoader/DotMidiLoader.cs
+++ YARG.Core/NewParsing/MidiLoader/DotMidiLoader.cs
@@ -104,7 +104,7 @@ namespace YARG.Core.NewParsing
         {
             var sync = new SyncTrack2(midi.TickRate);
 
-            var midiTrack = midi.LoadNextTrack()!;
+            using var midiTrack = midi.LoadNextTrack()!;
             string sequenceName = midiTrack.FindTrackName(Encoding.UTF8)!;
             while (midiTrack.ParseEvent())
             {
