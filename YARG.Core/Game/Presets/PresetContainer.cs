ceb4776916759939e526d39b36f3e51ad6a0f294
sonicfind <sonicfind2@gmail.com>
2024-06-17 17:40:32 -0500

    Implement PresetContainer json import/export
    + Remove readonly on the Guid (all the defaults are readonly anyways)

--- YARG.Core/Game/Presets/PresetContainer.cs
+++ YARG.Core/Game/Presets/PresetContainer.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using YARG.Core.Utility;
 
 namespace YARG.Core.Game
 {
+    public static class PresetJSON
+    {
+        public static readonly JsonSerializerSettings Settings = new()
+        {
+            Formatting = Formatting.Indented,
+            Converters = new List<JsonConverter>
+            {
+                new JsonColorConverter(),
+            }
+        };
+    }
+
     public struct PresetContainer<TPreset>
         where TPreset : struct
     {
-        public readonly Guid Id;
+        public Guid Id;
         public string Name;
         public TPreset Config;
 
@@ -20,11 +36,18 @@ namespace YARG.Core.Game
             Config = preset;
         }
 
-        public PresetContainer(Guid id, string name, in TPreset preset)
+        public readonly void Export(string path)
         {
-            Id = id;
-            Name = name;
-            Config = preset;
+            using var stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write);
+            using var writer = new StreamWriter(stream);
+            var text = JsonConvert.SerializeObject(this, PresetJSON.Settings);
+            writer.Write(text);
+        }
+
+        public static PresetContainer<TPreset> Import(string path)
+        {
+            string file = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<PresetContainer<TPreset>>(file, PresetJSON.Settings);
         }
     }
 }
