d390994ed5790c6140620712bfc86df2abc3955f
sonicfind <sonicfind2@gmail.com>
2024-06-20 10:40:46 -0500

    Make ParseSettings & SongMetadata constructors off SongEntry's Inisection code

--- YARG.Core/Song/Entries/Types/SongMetadata.cs
+++ YARG.Core/Song/Entries/Types/SongMetadata.cs
@@ -9,6 +9,7 @@ namespace YARG.Core.Song
 {
     public struct SongMetadata
     {
+        public const double MILLISECOND_FACTOR = 1000.0;
         public static readonly SortString DEFAULT_NAME = "Unknown Name";
         public static readonly SortString DEFAULT_ARTIST = "Unknown Artist";
         public static readonly SortString DEFAULT_ALBUM = "Unknown Album";
@@ -65,5 +66,100 @@ namespace YARG.Core.Song
         public int PlaylistTrack;
 
         public string LoadingPhrase;
+
+        public SongMetadata(IniSection modifiers, string defaultPlaylist)
+        {
+            modifiers.TryGet("name", out Name, DEFAULT_NAME);
+            modifiers.TryGet("artist", out Artist, DEFAULT_ARTIST);
+            modifiers.TryGet("album", out Album, DEFAULT_ALBUM);
+            modifiers.TryGet("genre", out Genre, DEFAULT_GENRE);
+
+            if (!modifiers.TryGet("year", out Year))
+            {
+                if (modifiers.TryGet("Year", out Year))
+                {
+                    if (Year.StartsWith(", "))
+                    {
+                        Year = Year[2..];
+                    }
+                    else if (Year.StartsWith(','))
+                    {
+                        Year = Year[1..];
+                    }
+                }
+                else
+                {
+                    Year = DEFAULT_YEAR;
+                }
+            }
+
+            if (!modifiers.TryGet("charter", out Charter, DEFAULT_CHARTER))
+            {
+                modifiers.TryGet("frets", out Charter, DEFAULT_CHARTER);
+            }
+
+            modifiers.TryGet("icon", out Source, DEFAULT_SOURCE);
+            modifiers.TryGet("playlist", out Playlist, defaultPlaylist);
+
+            modifiers.TryGet("loading_phrase", out LoadingPhrase);
+
+            if (!modifiers.TryGet("playlist_track", out PlaylistTrack))
+            {
+                PlaylistTrack = -1;
+            }
+
+            if (!modifiers.TryGet("album_track", out AlbumTrack))
+            {
+                AlbumTrack = -1;
+            }
+
+            modifiers.TryGet("song_length", out SongLength);
+            modifiers.TryGet("rating", out SongRating);
+
+            modifiers.TryGet("video_start_time", out VideoStartTime);
+            if (!modifiers.TryGet("video_end_time", out VideoEndTime))
+            {
+                VideoEndTime = -1;
+            }
+
+            if (!modifiers.TryGet("preview", out PreviewStart, out PreviewEnd))
+            {
+                if (!modifiers.TryGet("preview_start_time", out PreviewStart))
+                {
+                    // Capitlization = from .chart
+                    if (modifiers.TryGet("PreviewStart", out double previewStartSeconds))
+                    {
+                        PreviewStart = (long) (previewStartSeconds * MILLISECOND_FACTOR);
+                    }
+                    else
+                    {
+                        PreviewStart = -1;
+                    }
+                }
+
+                if (!modifiers.TryGet("preview_end_time", out PreviewEnd))
+                {
+                    // Capitlization = from .chart
+                    if (modifiers.TryGet("PreviewEnd", out double previewEndSeconds))
+                    {
+                        PreviewEnd = (long) (previewEndSeconds * MILLISECOND_FACTOR);
+                    }
+                    else
+                    {
+                        PreviewEnd = -1;
+                    }
+                }
+            }
+
+            if (!modifiers.TryGet("delay", out SongOffset) || SongOffset == 0)
+            {
+                if (modifiers.TryGet("Offset", out double songOffsetSeconds))
+                {
+                    SongOffset = (long) (songOffsetSeconds * MILLISECOND_FACTOR);
+                }
+            }
+
+            IsMaster = !modifiers.TryGet("tags", out string tag) || tag.ToLower() != "cover";
+        }
     }
 }
