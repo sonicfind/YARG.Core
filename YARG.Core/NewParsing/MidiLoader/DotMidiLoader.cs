658cb305ea04be2aec4ced297c7af52acbe895ca
sonicfind <sonicfind2@gmail.com>
2024-06-20 20:28:25 -0500

    Swap TextEvent's IsOccupied to IsEmpty

--- YARG.Core/NewParsing/MidiLoader/DotMidiLoader.cs
+++ YARG.Core/NewParsing/MidiLoader/DotMidiLoader.cs
@@ -196,7 +196,7 @@ namespace YARG.Core.NewParsing
         private static readonly byte[][] PREFIXES = { Encoding.ASCII.GetBytes("[section "), Encoding.ASCII.GetBytes("[prc_") };
         private static void LoadEventsTrack_Midi(TextEvents2 events, SyncTrack2 sync, YARGMidiTrack midiTrack)
         {
-            if (!events.IsOccupied())
+            if (!events.IsEmpty())
             {
                 YargLogger.LogInfo("EVENTS track appears multiple times. Not parsing repeats...");
                 return;
