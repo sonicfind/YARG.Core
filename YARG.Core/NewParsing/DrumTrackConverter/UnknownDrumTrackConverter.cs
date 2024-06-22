e85410cd200f81309b5ca18ce78adf1854b02ae3
sonicfind <sonicfind2@gmail.com>
2024-06-21 20:32:21 -0500

    DrumNote refactor
    + Change `IDrumConfig` indexer to return or take `DualTime`s (removes the ref warnings)
    + Move `NUM_PADS` const to the `IDrumPadConfig` scope
    + Remove `LoadFrom()` method from `IDrumNote` interface. The converters will now just use casting and memcopy to transfer data.
    + Split unknowndrums converter between the basic and pro drums configs

--- YARG.Core/NewParsing/DrumTrackConverter/UnknownDrumTrackConverter.cs
+++ YARG.Core/NewParsing/DrumTrackConverter/UnknownDrumTrackConverter.cs
@@ -6,39 +6,36 @@ namespace YARG.Core.NewParsing
 {
     internal static class UnknownDrumTrackConverter
     {
-        public static BasicInstrumentTrack2<TDrumNote> ConvertTo<TDrumNote, TPads>(this BasicInstrumentTrack2<ProDrumNote2<FiveLane>> source)
-            where TDrumNote : unmanaged, IDrumNote<TPads>
-            where TPads : unmanaged, IDrumPadConfig
+        public static BasicInstrumentTrack2<DrumNote2<TPads>> ConvertToBasic<TPads>(this BasicInstrumentTrack2<ProDrumNote2<FiveLane>> source)
+            where TPads : unmanaged, IDrumPadConfig<TPads>
         {
-            var newTrack = new BasicInstrumentTrack2<TDrumNote>
+            var newTrack = new BasicInstrumentTrack2<DrumNote2<TPads>>
             {
                 SpecialPhrases = source.SpecialPhrases,
                 Events = source.Events
             };
-            return ConvertTo<TDrumNote, TPads>(source, newTrack);
+            return ConvertToBasic(source, newTrack);
         }
 
-        public static BasicInstrumentTrack2<TDrumNote> ConvertTo<TDrumNote, TPads>(this BasicInstrumentTrack2<ProDrumNote2<FiveLane>> source, BasicInstrumentTrack2<TDrumNote> destination)
-            where TDrumNote : unmanaged, IDrumNote<TPads>
-            where TPads : unmanaged, IDrumPadConfig
+        public static BasicInstrumentTrack2<DrumNote2<TPads>> ConvertToBasic<TPads>(this BasicInstrumentTrack2<ProDrumNote2<FiveLane>> source, BasicInstrumentTrack2<DrumNote2<TPads>> destination)
+            where TPads : unmanaged, IDrumPadConfig<TPads>
         {
             for (int i = 0; i < InstrumentTrack2.NUM_DIFFICULTIES; ++i)
             {
                 var diff = source[i];
                 if (diff != null && diff.IsOccupied())
                 {
-                    destination[i] = ConvertTo<TDrumNote, TPads>(diff);
+                    destination[i] = ConvertToBasic<TPads>(diff);
                     source[i] = null;
                 }
             }
             return destination;
         }
 
-        private static DifficultyTrack2<TDrumNote> ConvertTo<TDrumNote, TPads>(DifficultyTrack2<ProDrumNote2<FiveLane>> source)
-            where TDrumNote : unmanaged, IDrumNote<TPads>
-            where TPads : unmanaged, IDrumPadConfig
+        private static DifficultyTrack2<DrumNote2<TPads>> ConvertToBasic<TPads>(DifficultyTrack2<ProDrumNote2<FiveLane>> source)
+            where TPads : unmanaged, IDrumPadConfig<TPads>
         {
-            var newDifficulty = new DifficultyTrack2<TDrumNote>()
+            var newDifficulty = new DifficultyTrack2<DrumNote2<TPads>>()
             {
                 SpecialPhrases = source.SpecialPhrases,
                 Events = source.Events
@@ -47,12 +44,59 @@ namespace YARG.Core.NewParsing
             newDifficulty.Notes.Capacity = source.Notes.Count;
             unsafe
             {
-                TDrumNote buffer = default;
                 var end = source.Notes.End;
                 for (var curr = source.Notes.Data; curr < end; ++curr)
                 {
-                    buffer.LoadFrom(curr->Value);
-                    // Append sends deep copies, so this is safe
+                    newDifficulty.Notes.Append(curr->Key, in *(DrumNote2<TPads>*) &curr->Value);
+                }
+            }
+            source.Notes.Dispose();
+            return newDifficulty;
+        }
+
+        public static BasicInstrumentTrack2<ProDrumNote2<FourLane>> ConvertToPro(this BasicInstrumentTrack2<ProDrumNote2<FiveLane>> source)
+        {
+            var newTrack = new BasicInstrumentTrack2<ProDrumNote2<FourLane>>
+            {
+                SpecialPhrases = source.SpecialPhrases,
+                Events = source.Events
+            };
+            return ConvertToPro(source, newTrack);
+        }
+
+        public static BasicInstrumentTrack2<ProDrumNote2<FourLane>> ConvertToPro(this BasicInstrumentTrack2<ProDrumNote2<FiveLane>> source, BasicInstrumentTrack2<ProDrumNote2<FourLane>> destination)
+        {
+            for (int i = 0; i < InstrumentTrack2.NUM_DIFFICULTIES; ++i)
+            {
+                var diff = source[i];
+                if (diff != null && diff.IsOccupied())
+                {
+                    destination[i] = ConvertToPro(diff);
+                    source[i] = null;
+                }
+            }
+            return destination;
+        }
+
+        private static DifficultyTrack2<ProDrumNote2<FourLane>> ConvertToPro(DifficultyTrack2<ProDrumNote2<FiveLane>> source)
+        {
+            var newDifficulty = new DifficultyTrack2<ProDrumNote2<FourLane>>()
+            {
+                SpecialPhrases = source.SpecialPhrases,
+                Events = source.Events
+            };
+
+            newDifficulty.Notes.Capacity = source.Notes.Count;
+            unsafe
+            {
+                ProDrumNote2<FourLane> buffer = default;
+                var end = source.Notes.End;
+                for (var curr = source.Notes.Data; curr < end; ++curr)
+                {
+                    Buffer.MemoryCopy(&curr->Value, &buffer, sizeof(ProDrumNote2<FourLane>), sizeof(DrumNote2<FourLane>));
+                    buffer.Cymbals.Yellow = curr->Value.Cymbals.Yellow;
+                    buffer.Cymbals.Blue   = curr->Value.Cymbals.Blue;
+                    buffer.Cymbals.Green  = curr->Value.Cymbals.Green;
                     newDifficulty.Notes.Append(curr->Key, in buffer);
                 }
             }
