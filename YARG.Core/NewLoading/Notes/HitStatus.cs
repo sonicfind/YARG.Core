88ae1cd5ac778dd1e8b4de282ed46b61af06b434
sonicfind <sonicfind2@gmail.com>
2024-06-26 21:33:31 -0500

    Add SubNote type & Hitstatus enum
    Basic starting block for the structure on engine-based notes

---
+++ YARG.Core/NewLoading/Notes/HitStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewLoading
+{
+    public enum HitStatus
+    {
+        Idle,
+        Hit,
+        Sustained,
+        Dropped,
+        Missed
+    }
+}
