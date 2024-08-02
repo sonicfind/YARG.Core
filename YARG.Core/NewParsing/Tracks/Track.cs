02906f0246066e74a66d2e01236b12cbb42eb268
sonicfind <sonicfind2@gmail.com>
2024-08-01 21:21:40 -0500

    Remove SpecialPhraseInfo structure
    Replaced by built-in valuetuple type

--- YARG.Core/NewParsing/Tracks/Track.cs
+++ YARG.Core/NewParsing/Tracks/Track.cs
@@ -4,9 +4,26 @@ using System.Text;
 
 namespace YARG.Core.NewParsing
 {
+    public enum SpecialPhraseType
+    {
+        FaceOff_Player1 = 0,
+        FaceOff_Player2 = 1,
+        StarPower = 2,
+        Solo = 3,
+        LyricLine = 4,
+        RangeShift = 5,
+        HarmonyLine = 6,
+        StarPower_Diff = 8,
+        BRE = 64,
+        Tremolo = 65,
+        Trill = 66,
+        LyricShift = 67,
+        Glissando = 68,
+    }
+
     public abstract class Track : IDisposable
     {
-        public YARGManagedSortedList<DualTime, Dictionary<SpecialPhraseType, SpecialPhraseInfo>> SpecialPhrases = new();
+        public YARGManagedSortedList<DualTime, Dictionary<SpecialPhraseType, (DualTime Duration, int Velocity)>> SpecialPhrases = new();
         public YARGManagedSortedList<DualTime, HashSet<string>> Events = new();
         protected bool disposedValue;
 
