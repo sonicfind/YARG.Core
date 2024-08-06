54174b212ff4a86463bfb9d1618603f7ebc541bc
sonicfind <sonicfind2@gmail.com>
2024-08-06 01:05:40 -0500

    Replace SongSection2 with NonNullString
    Functionally the same, but with a more generic usecase.

---
+++ YARG.Core/NewParsing/NonNullString.cs
@@ -0,0 +1,26 @@
+﻿namespace YARG.Core.NewParsing
+{
+    public class NonNullString
+    {
+        private string _str;
+
+        public string Str
+        {
+            get => _str;
+            set => _str = value ?? string.Empty;
+        }
+
+        public NonNullString()
+        {
+            _str = string.Empty;
+        }
+
+        public NonNullString(string str)
+        {
+            _str = str ?? string.Empty;
+        }
+
+        public static implicit operator string(NonNullString section) => section._str;
+        public static implicit operator NonNullString(string str) => new(str);
+    }
+}
