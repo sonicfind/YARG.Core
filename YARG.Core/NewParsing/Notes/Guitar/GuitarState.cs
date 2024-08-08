286463f7ba2d940b3c96ff97901866527e9fb6b5
sonicfind <sonicfind2@gmail.com>
2024-08-07 22:35:35 -0500

    Split note structures to separate files

---
+++ YARG.Core/NewParsing/Notes/Guitar/GuitarState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public enum GuitarState
+    {
+        Natural,
+        Forced,
+        Hopo,
+        Strum,
+        Tap
+    }
+}
