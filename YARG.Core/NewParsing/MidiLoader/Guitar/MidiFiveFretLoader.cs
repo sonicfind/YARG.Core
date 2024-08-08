be4d626e1f9f8c84ffc9540fda8b2ebc9a6cd5fa
sonicfind <sonicfind2@gmail.com>
2024-08-07 22:15:10 -0500

    Ignore trackname events in midiLoader functions

--- YARG.Core/NewParsing/MidiLoader/Guitar/MidiFiveFretLoader.cs
+++ YARG.Core/NewParsing/MidiLoader/Guitar/MidiFiveFretLoader.cs
@@ -456,7 +456,7 @@ namespace YARG.Core.NewParsing.Midi
                         }
                     }
                 }
-                else if (MidiEventType.Text <= stats.Type && stats.Type <= MidiEventType.Text_EnumLimit)
+                else if (MidiEventType.Text <= stats.Type && stats.Type <= MidiEventType.Text_EnumLimit && stats.Type != MidiEventType.Text_TrackName)
                 {
                     var str = midiTrack.ExtractTextOrSysEx();
                     if (laneIndices[0] == 13 && (str.SequenceEqual(ENHANCED_STRINGS[0]) || str.SequenceEqual(ENHANCED_STRINGS[1])))
--- YARG.Core/NewParsing/MidiLoader/ProKeys/MidiProKeysLoader.cs
+++ YARG.Core/NewParsing/MidiLoader/ProKeys/MidiProKeysLoader.cs
@@ -149,7 +149,7 @@ namespace YARG.Core.NewParsing.Midi
                         }
                     }
                 }
-                else if (MidiEventType.Text <= stats.Type && stats.Type <= MidiEventType.Text_EnumLimit)
+                else if (MidiEventType.Text <= stats.Type && stats.Type <= MidiEventType.Text_EnumLimit && stats.Type != MidiEventType.Text_TrackName)
                 {
                     var str = midiTrack.ExtractTextOrSysEx();
                     var ev = str.GetString(Encoding.ASCII);
