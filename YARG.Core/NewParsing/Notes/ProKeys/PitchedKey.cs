f4ac2dc6b7c0a22f24f5338e1ac7aff04704fc0b
sonicfind <sonicfind2@gmail.com>
2024-08-07 22:35:35 -0500

    Split note structures to separate files

---
+++ YARG.Core/NewParsing/Notes/ProKeys/PitchedKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.NewParsing
+{
+    public struct PitchedKey
+    {
+        public int Pitch;
+        public DualTime Duration;
+    }
+}
