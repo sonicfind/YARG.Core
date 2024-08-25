be09b093f24f0a82ee0a21e36b032588cfd3a3bd
sonicfind <sonicfind2@gmail.com>
2024-08-24 21:58:40 -0500

    Move GuitarState to IGuitarNote.cs

--- YARG.Core/NewParsing/Notes/Guitar/IGuitarNote.cs
+++ YARG.Core/NewParsing/Notes/Guitar/IGuitarNote.cs
@@ -4,6 +4,15 @@ using System.Text;
 
 namespace YARG.Core.NewParsing
 {
+    public enum GuitarState
+    {
+        Natural,
+        Forced,
+        Hopo,
+        Strum,
+        Tap
+    }
+
     public interface IGuitarNote : IInstrumentNote
     {
         public GuitarState State { get; set; }
