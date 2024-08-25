3f7c56e06b4e0c7c0795eae8aa5437d1248998cb
sonicfind <sonicfind2@gmail.com>
2024-08-24 22:20:46 -0500

    Initial round of documentation

--- YARG.Core/NewParsing/Tracks/ITrack.cs
+++ YARG.Core/NewParsing/Tracks/ITrack.cs
@@ -6,9 +6,26 @@ namespace YARG.Core.NewParsing
 {
     public interface ITrack : IDisposable
     {
+        /// <summary>
+        /// Returns whether the track contains no data
+        /// </summary>
+        /// <returns>Whether the track is empty</returns>
         public bool IsEmpty();
+
+        /// <summary>
+        /// Clears all data
+        /// </summary>
         public void Clear();
+
+        /// <summary>
+        /// Shrinks unmanaged data buffers to solely cover actual data
+        /// </summary>
         public void TrimExcess();
+
+        /// <summary>
+        /// Returns the end time for any notes present
+        /// </summary>
+        /// <returns>The tick and seconds position of the end time</returns>
         public DualTime GetLastNoteTime();
     }
 }
