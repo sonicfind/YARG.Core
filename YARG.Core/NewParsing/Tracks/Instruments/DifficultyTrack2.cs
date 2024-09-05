71e41b60342349e5e4478a5ab2f33f81c3a87f7e
sonicfind <sonicfind2@gmail.com>
2024-09-05 17:26:13 -0500

    Convert from PhraseTrack inheritance to InstrumentPhrases Composition
    Have InstrumentTrack2 & DifficultyTrack2 hold variables of the InstrumentPhrases type containing the phrases (and one for the separate text event lists).

--- YARG.Core/NewParsing/Tracks/Instruments/DifficultyTrack2.cs
+++ YARG.Core/NewParsing/Tracks/Instruments/DifficultyTrack2.cs
@@ -1,15 +1,16 @@
-﻿using Melanchall.DryWetMidi.MusicTheory;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using YARG.Core.Containers;
 
 namespace YARG.Core.NewParsing
 {
-    public class DifficultyTrack2<TNote> : PhraseTrack
+    public class DifficultyTrack2<TNote> : ITrack
         where TNote : unmanaged, IInstrumentNote
     {
         public readonly YARGNativeSortedList<DualTime, TNote> Notes = new();
+        public readonly InstrumentPhrases Phrases = new();
+        public readonly YARGManagedSortedList<DualTime, HashSet<string>> Events = new();
 
         public DifficultyTrack2() {}
 
@@ -19,40 +20,44 @@ namespace YARG.Core.NewParsing
         /// </summary>
         /// <remarks>Does not effect <see cref="Notes"/>. That remains unchanged</remarks>
         /// <param name="source">The source track to move phrases from</param>
-        public DifficultyTrack2(PhraseTrack source)
-            : base(source) {}
+        public DifficultyTrack2(InstrumentPhrases phrases, YARGManagedSortedList<DualTime, HashSet<string>> events)
+        {
+            Phrases = new(phrases);
+            Events = new(events);
+        }
 
         /// <summary>
         /// Returns if no notes, phrases, or events are present
         /// </summary>
         /// <returns>Whether the track is empty</returns>
-        public override bool IsEmpty()
+        public virtual bool IsEmpty()
         {
-            return Notes.IsEmpty() && base.IsEmpty();
+            return Notes.IsEmpty() && Phrases.IsEmpty() && Events.IsEmpty();
         }
 
         /// <summary>
         /// Clears all notes, phrases, and events
         /// </summary>
-        public override void Clear()
+        public virtual void Clear()
         {
             Notes.Clear();
-            base.Clear();
+            Phrases.Clear();
+            Events.Clear();
         }
 
         /// <summary>
         /// Trims excess unmanaged buffer data from notes and phrases
         /// </summary>
-        public override void TrimExcess()
+        public virtual void TrimExcess()
         {
             if ((Notes.Count < 500 || 10000 <= Notes.Count) && Notes.Count < Notes.Capacity)
             {
                 Notes.TrimExcess();
             }
-            base.TrimExcess();
+            Phrases.TrimExcess();
         }
 
-        public override unsafe DualTime GetLastNoteTime()
+        public unsafe DualTime GetLastNoteTime()
         {
             if (Notes.IsEmpty())
             {
@@ -66,10 +71,10 @@ namespace YARG.Core.NewParsing
         /// <summary>
         /// Diposes all the unmanaged data used for notes and phrases
         /// </summary>
-        public override void Dispose()
+        public virtual void Dispose()
         {
             Notes.Dispose();
-            base.Dispose();
+            Phrases.Dispose();
         }
     }
 }
