3ae21fb0f169086c40edfdd79f38e65e7f457a1c
sonicfind <sonicfind2@gmail.com>
2024-10-13 11:28:37 -0500

    Remove chordsnapper logs

--- YARG.Core/NewParsing/Midi/Guitar/MidiSixFretLoader.cs
+++ YARG.Core/NewParsing/Midi/Guitar/MidiSixFretLoader.cs
@@ -84,12 +84,7 @@ namespace YARG.Core.NewParsing.Midi
                     {
                         // If the distance between the current NoteOn and the previous NoteOn is less than a certain threshold
                         // the previous position will override the current one, to "chord" multiple notes together
-                        if (chordSnapper.Snap(ref position) && midiTrack.Position > 0)
-                        {
-#if DEBUG
-                            YargLogger.LogInfo("Snap occured");
-#endif
-                        }
+                        chordSnapper.Snap(ref position);
 
                         if (SIXFRET_MIN <= note.value && note.value <= SIXFRET_MAX)
                         {
@@ -370,12 +365,7 @@ namespace YARG.Core.NewParsing.Midi
                         // the previous position will override the current one, to "chord" multiple notes together
                         //
                         // While this isn't an actual NoteOn midi event, we should interpret it like one in this instance for safety.
-                        if (chordSnapper.Snap(ref position) && midiTrack.Position > 0)
-                        {
-#if DEBUG
-                            YargLogger.LogInfo("Snap occured");
-#endif
-                        }
+                        chordSnapper.Snap(ref position);
                     }
 
                     int diffIndex = sysex[SYSEX_DIFF_INDEX];
