7c991b7180d2d5770b88a27320b19a6742c4fdb2
sonicfind <sonicfind2@gmail.com>
2024-10-14 22:07:52 -0500

    Have incomplete VenueTrack2 implement ITrack

--- YARG.Core/NewParsing/Tracks/Venue/VenueTrack2.cs
+++ YARG.Core/NewParsing/Tracks/Venue/VenueTrack2.cs
@@ -6,12 +6,36 @@ using YARG.Core.Containers;
 
 namespace YARG.Core.NewParsing
 {
-    public class VenueTrack2 : IDisposable
+    public class VenueTrack2 : ITrack
     {
         public YARGNativeSortedList<DualTime, VenueEvent2<LightingType>> Lighting = YARGNativeSortedList<DualTime, VenueEvent2<LightingType>>.Default;
         public YARGNativeSortedList<DualTime, VenueEvent2<PostProcessingType>> PostProcessing = YARGNativeSortedList<DualTime, VenueEvent2<PostProcessingType>>.Default;
         public YARGNativeSortedList<DualTime, VenueEvent2<PerformerEvent2>> Performer = YARGNativeSortedList<DualTime, VenueEvent2<PerformerEvent2>>.Default;
         public YARGNativeSortedList<DualTime, VenueEvent2<StageEffect>> Stage = YARGNativeSortedList<DualTime, VenueEvent2<StageEffect>>.Default;
+
+        public bool IsEmpty()
+        {
+            return Lighting.IsEmpty()
+                && PostProcessing.IsEmpty()
+                && Performer.IsEmpty()
+                && Stage.IsEmpty();
+        }
+
+        public void TrimExcess()
+        {
+            Lighting.TrimExcess();
+            PostProcessing.TrimExcess();
+            Performer.TrimExcess();
+            Stage.TrimExcess();
+        }
+
+        public void Clear()
+        {
+            Lighting.Clear();
+            PostProcessing.Clear();
+            Performer.Clear();
+            Stage.Clear();
+        }
 
         public void Dispose()
         {
