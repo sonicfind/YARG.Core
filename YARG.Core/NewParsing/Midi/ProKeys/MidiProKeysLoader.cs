6acf94a7bb0df2ff860cdc27c3c5f696b022a482
sonicfind <sonicfind2@gmail.com>
2024-10-14 22:04:20 -0500

    Only parse ProKeys text events in Expert

--- YARG.Core/NewParsing/Midi/ProKeys/MidiProKeysLoader.cs
+++ YARG.Core/NewParsing/Midi/ProKeys/MidiProKeysLoader.cs
@@ -161,7 +161,7 @@ namespace YARG.Core.NewParsing.Midi
                         }
                     }
                 }
-                else if (MidiEventType.Text <= stats.Type && stats.Type <= MidiEventType.Text_EnumLimit && stats.Type != MidiEventType.Text_TrackName)
+                else if (diffIndex == 3 && MidiEventType.Text <= stats.Type && stats.Type <= MidiEventType.Text_EnumLimit && stats.Type != MidiEventType.Text_TrackName)
                 {
                     // Unless, for some stupid-ass reason, this track contains lyrics,
                     // all actually useful events will utilize ASCII encoding for state
