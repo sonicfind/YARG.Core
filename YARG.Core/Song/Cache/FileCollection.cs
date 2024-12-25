4f8ee91ad816fb64a0c0fc6d94e62435fe7197c5
sonicfind <sonicfind2@gmail.com>
2024-12-24 20:19:00 -0500

    Rework FileCollection type
    + Combine the subfile and subdirectory dictionary variables into a single collection. Allows a simpler ForEach pattern when iterated over externally.
    
    + Add `FindFile`, `FindDirectory`, and `ContainsDirectory` methods for encapsulation & simplified access.

--- YARG.Core/Song/Cache/FileCollection.cs
+++ YARG.Core/Song/Cache/FileCollection.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 namespace YARG.Core.Song.Cache
@@ -6,8 +8,7 @@ namespace YARG.Core.Song.Cache
     public readonly struct FileCollection
     {
         public readonly DirectoryInfo Directory;
-        public readonly Dictionary<string, FileInfo> Subfiles;
-        public readonly Dictionary<string, DirectoryInfo> SubDirectories;
+        public readonly Dictionary<string, FileSystemInfo> Entries;
         public readonly bool ContainedDupes;
 
         internal static bool TryCollect(string directory, out FileCollection collection)
@@ -25,49 +26,64 @@ namespace YARG.Core.Song.Cache
         internal FileCollection(DirectoryInfo directory)
         {
             Directory = directory;
-            Subfiles = new();
-            SubDirectories = new();
-            var dupedFiles = new HashSet<string>();
-            var dupedDirectories = new HashSet<string>();
+            Entries = new Dictionary<string, FileSystemInfo>(StringComparer.InvariantCultureIgnoreCase);
+            var dupes = new HashSet<string>();
 
-            foreach (var info in directory.EnumerateFileSystemInfos())
+            foreach (var entry in directory.EnumerateFileSystemInfos())
             {
-                string name = info.Name.ToLowerInvariant();
-                switch (info)
+                if (!Entries.TryAdd(entry.Name, entry))
                 {
-                    case FileInfo subFile:
-                        if (!Subfiles.TryAdd(name, subFile))
-                        {
-                            dupedFiles.Add(name);
-                        }
-                        break;
-                    case DirectoryInfo subDirectory:
-                        if (!SubDirectories.TryAdd(name, subDirectory))
-                        {
-                            dupedDirectories.Add(name);
-                        }
-                        break;
+                    dupes.Add(entry.Name);
                 }
             }
 
             // Removes any sort of ambiguity from duplicates
-            ContainedDupes = dupedFiles.Count > 0 || dupedDirectories.Count > 0;
-            foreach (var dupe in dupedFiles)
+            ContainedDupes = dupes.Count > 0;
+            foreach (var dupe in dupes)
             {
-                Subfiles.Remove(dupe);
+                Entries.Remove(dupe);
             }
+        }
 
-            foreach (var dupe in dupedDirectories)
+        public bool FindFile(string name, out FileInfo file)
+        {
+            if (Entries.TryGetValue(name, out var entry) && entry is FileInfo result)
             {
-                SubDirectories.Remove(dupe);
+                file = result;
+                return true;
             }
+            file = null!;
+            return false;
+        }
+
+        public bool FindDirectory(string name, out DirectoryInfo directory)
+        {
+            if (Entries.TryGetValue(name, out var entry) && entry is DirectoryInfo result)
+            {
+                directory = result;
+                return true;
+            }
+            directory = null!;
+            return false;
+        }
+
+        public bool ContainsDirectory()
+        {
+            foreach (var entry in Entries)
+            {
+                if (entry.Value.Attributes == FileAttributes.Directory)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool ContainsAudio()
         {
-            foreach (var subFile in Subfiles.Keys)
+            foreach (var entry in Entries)
             {
-                if (IniAudio.IsAudioFile(subFile))
+                if (IniAudio.IsAudioFile(entry.Key))
                 {
                     return true;
                 }
