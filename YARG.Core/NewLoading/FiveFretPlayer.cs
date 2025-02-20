f44246bef9ed8b108a861983949017d8fb7dec03
sonicfind <sonicfind2@gmail.com>
2025-02-20 15:15:04 -0600

    Add basic outline for player loading with a base YargPlayer

---
+++ YARG.Core/NewLoading/FiveFretPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using YARG.Core.NewParsing;
+
+namespace YARG.Core.NewLoading
+{
+    public class FiveFretPlayer : YargPlayer
+    {
+        public static FiveFretPlayer Create(YARGChart chart, InstrumentTrack2<GuitarNote<FiveFret>> instrument, in DualTime endTime, in InstrumentSelection selection)
+        {
+            return new FiveFretPlayer();
+        }
+    }
+}
