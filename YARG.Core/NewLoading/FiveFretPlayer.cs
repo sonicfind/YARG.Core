20101879192b9d8586c85415ed75c52755689bfd
sonicfind <sonicfind2@gmail.com>
2025-02-20 23:38:10 -0600

    Add lane constants

--- YARG.Core/NewLoading/FiveFretPlayer.cs
+++ YARG.Core/NewLoading/FiveFretPlayer.cs
@@ -5,6 +5,17 @@ namespace YARG.Core.NewLoading
 {
     public class FiveFretPlayer : YargPlayer
     {
+        /// <summary>
+        /// Total number of possible lanes (including open note)
+        /// </summary>
+        public const int NUM_LANES = 6;
+        public const int OPEN_NOTE = 0;
+        public const int GREEN_NOTE = 1;
+        public const int RED_NOTE = 2;
+        public const int YELLOW_NOTE = 3;
+        public const int BLUE_NOTE = 4;
+        public const int ORANGE_NOTE = 5;
+
         public struct GuitarNoteGroup
         {
             public GuitarState State;
