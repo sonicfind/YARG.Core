fd8934a19ed8f5167d49d9fab27f3a2a78da8e13
sonicfind <sonicfind2@gmail.com>
2025-02-20 23:37:56 -0600

    Switch NoteIndex & LaneCount (now NoteCount) to `long`

--- YARG.Core/NewLoading/FiveFretPlayer.cs
+++ YARG.Core/NewLoading/FiveFretPlayer.cs
@@ -8,8 +8,8 @@ namespace YARG.Core.NewLoading
         public struct GuitarNoteGroup
         {
             public GuitarState State;
-            public int         NoteIndex;
-            public int         LaneCount;
+            public long        NoteIndex;
+            public long        NoteCount;
         }
 
         public static FiveFretPlayer Create(YARGChart chart, InstrumentTrack2<GuitarNote<FiveFret>> instrument, in DualTime endTime, in InstrumentSelection selection)
