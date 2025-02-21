826996c673306f9937e2f4dd1d4865eb4be88394
sonicfind <sonicfind2@gmail.com>
2025-02-20 21:35:39 -0600

    Add GuitarNoteGroup structure
    Handles mapping one or more BasicNote instances to a chord

--- YARG.Core/NewLoading/FiveFretPlayer.cs
+++ YARG.Core/NewLoading/FiveFretPlayer.cs
@@ -5,6 +5,13 @@ namespace YARG.Core.NewLoading
 {
     public class FiveFretPlayer : YargPlayer
     {
+        public struct GuitarNoteGroup
+        {
+            public GuitarState State;
+            public int         NoteIndex;
+            public int         LaneCount;
+        }
+
         public static FiveFretPlayer Create(YARGChart chart, InstrumentTrack2<GuitarNote<FiveFret>> instrument, in DualTime endTime, in InstrumentSelection selection)
         {
             return new FiveFretPlayer();
