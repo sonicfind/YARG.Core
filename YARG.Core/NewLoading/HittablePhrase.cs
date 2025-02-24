361831c33aba1050daaaa24f5bb5303b5fcc0b9f
sonicfind <sonicfind2@gmail.com>
2025-02-23 20:12:09 -0600

    Remove `HittablePhrase.HitCount`

--- YARG.Core/NewLoading/HittablePhrase.cs
+++ YARG.Core/NewLoading/HittablePhrase.cs
@@ -8,14 +8,12 @@ namespace YARG.Core.NewLoading
     public struct HittablePhrase
     {
         public DualTime EndTime;
-        public long TotalNotes;
-        public long HitCount;
+        public long     TotalNotes;
 
         public HittablePhrase(in DualTime endTime)
         {
             EndTime = endTime;
             TotalNotes = 0;
-            HitCount = 0;
         }
     }
 }
