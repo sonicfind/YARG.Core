7a646b9dbff8711d864673d9d1d9cecf3c1befe3
sonicfind <sonicfind2@gmail.com>
2025-02-23 19:57:19 -0600

    Move `InstrumentSelection` to a separate file

---
+++ YARG.Core/NewLoading/InstrumentSelection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using YARG.Core.Game;
+
+namespace YARG.Core.NewLoading
+{
+    public struct InstrumentSelection : IEquatable<InstrumentSelection>
+    {
+        public Instrument Instrument;
+        public Difficulty Difficulty;
+        public Modifier   Modifiers;
+
+        public readonly override int GetHashCode()
+        {
+            return Instrument.GetHashCode() ^ Difficulty.GetHashCode() ^ Modifiers.GetHashCode();
+        }
+
+        public readonly bool Equals(InstrumentSelection other)
+        {
+            return Instrument == other.Instrument &&
+                Difficulty == other.Difficulty &&
+                Modifiers  == other.Modifiers;
+        }
+    }
+}
